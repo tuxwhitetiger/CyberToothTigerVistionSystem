febf60dbf7d51c1ea806620c6a9ce944663ec020
tuxwhitetiger <tuxwhitetiger@gmail.com>
2022-12-12 05:49:52 +0000

    .

--- CyberToothTigerVistionSystem/network.cpp
+++ CyberToothTigerVistionSystem/network.cpp
@@ -32,18 +32,20 @@ int static network(std::string IP, int mysock)
         while (1) {
             std::cout <<"test"<< IP << ":" << sock << '\n';
             struct sockaddr_in serv_addr;
+
             if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
                 printf("\n Socket creation error \n");
                 return -1;
             }
-
+            std::cout << "test2" << IP << ":" << sock << '\n';
             serv_addr.sin_family = AF_INET;
             serv_addr.sin_port = htons(sock);
+            std::cout << "test3" << IP << ":" << sock << '\n';
 
             // Convert IPv4 and IPv6 addresses from text to binary form
             if (inet_pton(AF_INET, IP.c_str(), &serv_addr.sin_addr) <= 0) {
                 printf("\nInvalid address/ Address not supported \n");
-                std::cout << IP << '\n';
+                std::cout << IP << ":" << sock << '\n';
                 return -2;
             }
 
